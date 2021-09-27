82d4d631d8c6768619b3966ce8e6aa133f1c50d6
beslintony <beslin-tony.mathews@informatik.hs-fulda.de>
2021-09-28 01:25:57 +0200

    update template file and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import './index.css';
+
+import App from './App';
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
